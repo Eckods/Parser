f7470a4cd67e7180a57c8f315a37c9055f833980
RJ Esquivias <esquiviasrj@gmail.com>
2017-04-27 01:03:42 -0700

    Update
    Added : symbols, symbol array map, 3d prediction matrix, initMatrix
    function, LL Parsing Stack
    
    ToDoNext: Finish filling in 3dMatrix via initMatrix function, establish
    connection between LLStack and inputStream, Figure out how to
    create/print PST before conversion to AST

--- parser.cpp
+++ parser.cpp
@@ -1,20 +1,98 @@
 #include <iostream>
 #include <fstream>
+#include <stack>
+#include <list>
 using namespace std;
 
+enum symbols {Pgm, Block, Stmts, Stmt, Astmt, Y, Ostmt,
+              Wstmt, Fstmt, Else2, Elist, Elist2, Eprime, E,
+              Tprime, T, F, Pexpr, Fatom, Opadd, Opmul, $, 
+              kwdprog, brace1, brace2, semi, id, equal, kwdinput,
+              kwdprint, paren1, paren2, kwdwhile, kwdif, kwdelseif,
+              kwdelse, comma, SymbInt, SymbFloat, string, plus, minus, aster,
+              slash, caret, eps};
+
+const char* symNames[] = {"Pgm", "Block", "Stmts", "Stmt", "Astmt", 
+                          "Y", "Ostmt", "Wstmt", "Fstmt", "Else2",
+                          "Elist", "Elist2", "Eprime", "E", "Tprime",
+                          "T", "F", "Pexpr", "Fatom", "Opadd", "Opmul", "$", 
+                          "kwdprog", "brace1", "brace2", "semi", "id", "equal", 
+                          "kwdinput", "kwdprint", "paren1", "paren2",
+                          "kwdwhile", "kwdif", "kwdelseif", "kwdelse",
+                          "comma", "SymbInt", "SymbFloat", "string",
+                          "plus", "minus", "aster", "slash", "caret", "epsilon"};
+
+//3d LL Prediction Matrix
+list<symbols> Matrix[23][21];
+
+//Function Prototypes
 void readStream();
+void initMatrix(list<symbols> box[][21]);
 
 int main()
 {
+    initMatrix(Matrix);    
 
-    readStream();
+    //Set up LL Parsing Stack
+    stack<symbols> LLStack;
+
+    //Bottom of stack is always $
+    //Followed by Start Symbol (Pgm)
+    LLStack.push(symbols::$);
+    LLStack.push(symbols::Pgm);
+
+    //Example code showing access and printing from matrix
+    cout << symNames[Matrix[0][0].front()] << endl;
+    Matrix[0][0].pop_front();
+    cout << symNames[Matrix[0][0].front()] << endl;
+    Matrix[0][0].pop_front();
+
+    //readStream();
 
     return 0;
 }
 
+void initMatrix(list<symbols> box[][21])
+{
+    box[0][0].push_front(symbols::Block);
+    box[0][0].push_front(symbols::kwdprog);
+
+    box[1][1].push_front(symbols::brace2);
+    box[1][1].push_front(symbols::Stmts);
+    box[1][1].push_front(symbols::brace1);
+
+    box[2][2].push_front(symbols::eps);
+
+    box[2][4].push_front(symbols::Stmts);
+    box[2][4].push_front(symbols::semi);
+    box[2][4].push_front(symbols::Stmts);
+
+    box[2][7].push_front(symbols::Stmts);
+    box[2][7].push_front(symbols::semi);
+    box[2][7].push_front(symbols::Stmts);
+
+    box[2][10].push_front(symbols::Stmts);
+    box[2][10].push_front(symbols::semi);
+    box[2][10].push_front(symbols::Stmts);
+
+    box[2][11].push_front(symbols::Stmts);
+    box[2][11].push_front(symbols::semi);
+    box[2][11].push_front(symbols::Stmts);
+
+    box[3][4].push_front(symbols::Astmt);
+
+    box[3][7].push_front(symbols::Ostmt);
+
+    box[3][10].push_front(symbols::Wstmt);
+
+    box[3][11].push_front(symbols::Fstmt);
+
+    //TODO: FILL IN REST OF VALUES  
+}
+
 void readStream()
 {
-
+    char x;
     ifstream input("stream.txt");
 
     if(!input.is_open())
@@ -23,7 +101,6 @@ void readStream()
         exit(EXIT_FAILURE);
     }
     
-    char x;
     while(input.get(x))
     {
        cout << x;
