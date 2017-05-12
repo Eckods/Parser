c0cc3d36b5d99f06048ecbe3b95686004ab30dbd
dmoreno757 <dmoreno757@gmail.com>
2017-05-12 12:16:44 -0700

    Finished rules

--- main.cpp
+++ main.cpp
@@ -19,8 +19,8 @@ struct Tokens
 struct Syms {
 	string name;	// name of symbol
 	bool isTerm;	// is symbol term or not
-	//vector<Syms> kids;	// kids
-	//int kidCount;	// number of kids
+					//vector<Syms> kids;	// kids
+					//int kidCount;	// number of kids
 };
 
 struct Node {
@@ -50,7 +50,7 @@ int ParseMatrix[21][23] =
 	/*Else2*/{ 0,	0,	0,	15,	0,	0,	0,	0,	0,	0,	0,	0,	16,	17,	0,	0,	0,	0,	0,	0,	0,	0,	0 },
 	/*Elist*/{ 0,	0,	0,	0,	18,	0,	0,	0,	18,	19,	0,	0,	0,	0,	0,	18,	18,	18,	0,	0,	0,	0,	0 },
 	/*Elist2*/{ 0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	20,	0,	0,	0,	0,	0,	0,	0,	0 },
-	/*E'*/{ 0,	0,	0,	20,	0,	0,	0,	0,	0,	21,	0,	0,	0,	0,	21,	0,	0,	0,	22,	22,	0,	0,	0 },
+	/*E'*/{ 0,	0,	0,	21,	0,	0,	0,	0,	0,	21,	0,	0,	0,	0,	21,	0,	0,	0,	22,	22,	0,	0,	0 },
 	/*E*/{ 0,	0,	0,	0,	23,	0,	0,	0,	23,	0,	0,	0,	0,	0,	0,	23,	23,	23,	0,	0,	0,	0,	0 },
 	/*T'*/{ 0,	0,	0,	24,	0,	0,	0,	0,	0,	24,	0,	0,	0,	0,	24,	0,	0,	0,	24,	24,	25,	25,	25 },
 	/*T*/{ 0,	0,	0,	0,	26,	0,	0,	0,	26,	0,	0,	0,	0,	0,	0,	26,	26,	26,	0,	0,	0,	0,	0 },
@@ -75,7 +75,7 @@ int main()
 	lexer.runLexer();
 	system("cls"); // System call, I know, I know
 
-	// Tokenize the stream file
+				   // Tokenize the stream file
 	cout << "Converting lexer output into tokens" << endl;
 	tokens = tokenize();
 
@@ -123,202 +123,354 @@ void parseMachine()
 void performRule(int rule)
 {
 
-	switch (rule) 
+	switch (rule)
 	{
-		case 1: // Pgm = kwdprog Block
-		{
-			Syms Block = { "Block", false };
-			Syms kwdprog = { "kwdprog", true };
+	case 1: // Pgm = kwdprog Block
+	{
+		Syms Block = { "Block", false };
+		Syms kwdprog = { "kwdprog", true };
 
-			LLStack.pop();
-			LLStack.push(Block);
-			LLStack.push(kwdprog);
-			break;
-		}
-		case 2: // Block = brace1 Stmts brace2
-		{
-			Syms brace1 = { "brace1", true };
-			Syms Stmts = { "Stmts", false };
-			Syms brace2 = { "brace2", true };
+		LLStack.pop();
+		LLStack.push(Block);
+		LLStack.push(kwdprog);
+		break;
+	}
+	case 2: // Block = brace1 Stmts brace2
+	{
+		Syms brace1 = { "brace1", true };
+		Syms Stmts = { "Stmts", false };
+		Syms brace2 = { "brace2", true };
 
-			LLStack.pop();
-			LLStack.push(brace2);
-			LLStack.push(Stmts);
-			LLStack.push(brace1);
-			break;
-		}
-		case 3: // Stmts = eps
-		{
-			LLStack.pop();
-			break;
-		}
-		case 4: // Stmts = Stmt semi Stmt
-		{
-			Syms Stmt = { "Stmt", false };
-			Syms semi = { "semi", true };
-			Syms Stmts = { "Stmts", false };
+		LLStack.pop();
+		LLStack.push(brace2);
+		LLStack.push(Stmts);
+		LLStack.push(brace1);
+		break;
+	}
+	case 3: // Stmts = eps
+	{
+		LLStack.pop();
+		break;
+	}
+	case 4: // Stmts = Stmt semi Stmt
+	{
+		Syms Stmt = { "Stmt", false };
+		Syms semi = { "semi", true };
+		Syms Stmts = { "Stmts", false };
 
-			LLStack.pop();
-			LLStack.push(Stmts);
-			LLStack.push(semi);
-			LLStack.push(Stmt);
-			break;
-		}
-		case 5: //Stmt = Astmt
-		{
-			Syms Astmt = { "Astmt", false };
+		LLStack.pop();
+		LLStack.push(Stmts);
+		LLStack.push(semi);
+		LLStack.push(Stmt);
+		break;
+	}
+	case 5: //Stmt = Astmt
+	{
+		Syms Astmt = { "Astmt", false };
 
-			LLStack.pop();
-			LLStack.push(Astmt);
-			break;
-		}
-		case 6: // Stmt = Ostmt
-		{
-			Syms Ostmt = { "Ostmt", false };
+		LLStack.pop();
+		LLStack.push(Astmt);
+		break;
+	}
+	case 6: // Stmt = Ostmt
+	{
+		Syms Ostmt = { "Ostmt", false };
 
-			LLStack.pop();
-			LLStack.push(Ostmt);
-			break;
-		}
+		LLStack.pop();
+		LLStack.push(Ostmt);
+		break;
+	}
 
-		case 7: // Stmt = Wstmt
-		{
-			Syms Wstmt = { "Wstmt", false };
+	case 7: // Stmt = Wstmt
+	{
+		Syms Wstmt = { "Wstmt", false };
 
-			LLStack.pop();
-			LLStack.push(Wstmt);
-			break;
-		}
-		case 8: // Stmt = Fstmt
-		{
-			Syms Fstmt = { "Fstmt", false };
+		LLStack.pop();
+		LLStack.push(Wstmt);
+		break;
+	}
+	case 8: // Stmt = Fstmt
+	{
+		Syms Fstmt = { "Fstmt", false };
 
-			LLStack.pop();
-			LLStack.push(Fstmt);
-			break;
-		}
-		case 9: // Astmt = id equal Y
-		{
-			Syms id = { "id", true };
-			Syms equal = { "equal", true };
-			Syms Y = { "Y", false };
+		LLStack.pop();
+		LLStack.push(Fstmt);
+		break;
+	}
+	case 9: // Astmt = id equal Y
+	{
+		Syms id = { "id", true };
+		Syms equal = { "equal", true };
+		Syms Y = { "Y", false };
 
-			LLStack.pop();
-			LLStack.push(Y);
-			LLStack.push(equal);
-			LLStack.push(id);
-			break;
-		}
-		case 10: // Y = E
-		{
-			Syms E = { "E", false };
+		LLStack.pop();
+		LLStack.push(Y);
+		LLStack.push(equal);
+		LLStack.push(id);
+		break;
+	}
+	case 10: // Y = E
+	{
+		Syms E = { "E", false };
 
-			LLStack.pop();
-			LLStack.push(E);
-			break;
-		}
-		case 11: // Y = kwdinput
-		{
-			Syms kwdinput = { "kwdinput", true };
+		LLStack.pop();
+		LLStack.push(E);
+		break;
+	}
+	case 11: // Y = kwdinput
+	{
+		Syms kwdinput = { "kwdinput", true };
 
-			LLStack.pop();
-			LLStack.push(kwdinput);
-			break;
-		}
-		case 12: // Ostmt = kwdprint paren1 Elist paren2
-		{
-			Syms kwdprint = { "kwdprint", true };
-			Syms paren1 = { "parens1", true };
-			Syms Elist = { "Elist", false };
-			Syms paren2 = { "parens2", true };
+		LLStack.pop();
+		LLStack.push(kwdinput);
+		break;
+	}
+	case 12: // Ostmt = kwdprint paren1 Elist paren2
+	{
+		Syms kwdprint = { "kwdprint", true };
+		Syms paren1 = { "parens1", true };
+		Syms Elist = { "Elist", false };
+		Syms paren2 = { "parens2", true };
 
-			LLStack.pop();
-			LLStack.push(paren2);
-			LLStack.push(Elist);
-			LLStack.push(paren1);
-			LLStack.push(kwdprint);
-			break;
-		}
-		case 13: // Wstmt = kwdwhile Pexpr Block
-		{
-			Syms kwdwhile = { "kwdwhile", true };
-			Syms Pexpr = { "Pexpr", false };
-			Syms Block = { "Block", false };
+		LLStack.pop();
+		LLStack.push(paren2);
+		LLStack.push(Elist);
+		LLStack.push(paren1);
+		LLStack.push(kwdprint);
+		break;
+	}
+	case 13: // Wstmt = kwdwhile Pexpr Block
+	{
+		Syms kwdwhile = { "kwdwhile", true };
+		Syms Pexpr = { "Pexpr", false };
+		Syms Block = { "Block", false };
 
-			LLStack.pop();
-			LLStack.push(Block);
-			LLStack.push(Pexpr);
-			LLStack.push(kwdwhile);
-			break;
-		}
-		case 14: // Fstmt = kwdif Pexpr Block Else2
-		{
-			Syms kwdif = { "kwdif", true };
-			Syms Pexpr = { "Pexpr", false };
-			Syms Block = { "Block", false };
-			Syms Else2 = { "Else2", false };
+		LLStack.pop();
+		LLStack.push(Block);
+		LLStack.push(Pexpr);
+		LLStack.push(kwdwhile);
+		break;
+	}
+	case 14: // Fstmt = kwdif Pexpr Block Else2
+	{
+		Syms kwdif = { "kwdif", true };
+		Syms Pexpr = { "Pexpr", false };
+		Syms Block = { "Block", false };
+		Syms Else2 = { "Else2", false };
 
-			LLStack.pop();
-			LLStack.push(Else2);
-			LLStack.push(Block);
-			LLStack.push(Pexpr);
-			LLStack.push(kwdif);
-			break;
-		}
-		case 15: // Else2 = eps
-		{
-			LLStack.pop();
-			break;
-		}
-		case 16: //Else2 = kwdelseif Pexpr Block Else2
-		{
-			Syms kwdelseif = { "kwdelseif", true };
-			Syms Pexpr = { "Pexpr", false };
-			Syms Block = { "Block", false };
-			Syms Else2 = { "Else2", false };
+		LLStack.pop();
+		LLStack.push(Else2);
+		LLStack.push(Block);
+		LLStack.push(Pexpr);
+		LLStack.push(kwdif);
+		break;
+	}
+	case 15: // Else2 = eps
+	{
+		LLStack.pop();
+		break;
+	}
+	case 16: //Else2 = kwdelseif Pexpr Block Else2
+	{
+		Syms kwdelseif = { "kwdelseif", true };
+		Syms Pexpr = { "Pexpr", false };
+		Syms Block = { "Block", false };
+		Syms Else2 = { "Else2", false };
 
-			LLStack.pop();
-			LLStack.push(Else2);
-			LLStack.push(Block);
-			LLStack.push(Pexpr);
-			LLStack.push(kwdelseif);
-			break;
-		}
-		case 17: //Else2 = kwdelse Block
-		{
-			Syms kwdelse = { "kwdelse", true };
-			Syms Block = { "Block", false };
+		LLStack.pop();
+		LLStack.push(Else2);
+		LLStack.push(Block);
+		LLStack.push(Pexpr);
+		LLStack.push(kwdelseif);
+		break;
+	}
+	case 17: //Else2 = kwdelse Block
+	{
+		Syms kwdelse = { "kwdelse", true };
+		Syms Block = { "Block", false };
 
-			LLStack.pop();
-			LLStack.push(Block);
-			LLStack.push(kwdelse);
-			break;
-		}
-		case 18: // Elist = E Elist2
-		{
-			Syms E = { "E", false };
-			Syms Elist2 = { "Elist2", false};
+		LLStack.pop();
+		LLStack.push(Block);
+		LLStack.push(kwdelse);
+		break;
+	}
+	case 18: // Elist = E Elist2
+	{
+		Syms E = { "E", false };
+		Syms Elist2 = { "Elist2", false };
 
-			LLStack.pop();
-			LLStack.push(Elist2);
-			LLStack.push(E);
-			break;
-		}
-		case 19: // Elist = eps
-		{
-			LLStack.pop();
-			break;
-		}
-		case 20: // Elist2 = comma Elist
-		{
-			Syms comma = { "comma", true };
-			Syms Elist = { "Elist", false };
+		LLStack.pop();
+		LLStack.push(Elist2);
+		LLStack.push(E);
+		break;
+	}
+	case 19: // Elist = eps
+	{
+		LLStack.pop();
+		break;
+	}
+	case 20: // Elist2 = eps
+	{
+		LLStack.pop();
+		break;
+	}
+	/*********************New Shit******************************/
+	case 21: //E'= eps
+	{
+		LLStack.pop();
+		break;
+	}
+	case 22: //E' = Opadd T E'
+	{
+		Syms Opadd = { "Opadd", false };
+		Syms T = { "T", false };
+		Syms Eprime = { "E'", false };
 
-			LLStack.pop();
-			LLStack.push(Elist);
-			LLStack.push(comma);
-			break;
-		}
+		LLStack.pop();
+		LLStack.push(Eprime);
+		LLStack.push(T);
+		LLStack.push(Opadd);
+		break;
+	}
+	case 23: // E = T E'
+	{
+		Syms T = { "T", false };
+		Syms Eprime = { "E'", false };
+
+		LLStack.pop();
+		LLStack.push(Eprime);
+		LLStack.push(T);
+		break;
+	}
+	case 24: // T'= eps
+	{
+		LLStack.pop();
+		break;
+	}
+	case 25: // T'=Opmul F T'
+	{
+		Syms Opmul = { "Opmul", false };
+		Syms F = { "F'", false };
+		Syms Tprime{ "T'", false };
+
+		LLStack.pop();
+		LLStack.push(Tprime);
+		LLStack.push(F);
+		LLStack.push(Opmul);
+		break;
+	}
+	case 26: // T = FT'
+	{
+		Syms F = { "F'", false };
+		Syms Tprime{ "T'", false };
+
+		LLStack.pop();
+		LLStack.push(Tprime);
+		LLStack.push(F);
+		break;
+	}
+	case 27: //F = Fatom
+	{
+		Syms Fatom = { "Fatom", false };
+
+		LLStack.pop();
+		LLStack.push(Fatom);
+		break;
+	}
+	case 28: // F = Pexpr
+	{
+		Syms Pexpr = { "Pexpr", false };
+
+		LLStack.pop();
+		LLStack.push(Pexpr);
+		break;
+	}
+	case 29: // Pexpr = paren1 E paren2
+	{
+		Syms paren1 = { "paren1", true };
+		Syms E{ "E", false };
+		Syms paren2 = { "paren2", true };
+
+		LLStack.pop();
+		LLStack.push(paren2);
+		LLStack.push(E);
+		LLStack.push(paren1);
+		break;
+	}
+	case 30: // Fatom = id
+	{
+		Syms id = { "id", true };
+
+		LLStack.pop();
+		LLStack.push(id);
+		break;
+	}
+	case 31: // Fatom = int /************************************** CHECK if I did this int right********/
+	{
+		Syms integer = { "int", true };
+
+		LLStack.pop();
+		LLStack.push(integer);
+		break;
+	}
+	case 32: // Fatom = float
+	{
+		Syms floatN = { "floar", true };
+
+		LLStack.pop();
+		LLStack.push(floatN);
+		break;
+	}
+	case 33: // Fatom = string
+	{
+		Syms stringl = { "strin", true };
+
+		LLStack.pop();
+		LLStack.push(stringl);
+		break;
+	}
+	case 34: // Opadd = plus
+	{
+		Syms plus = { "plus", true };
+
+		LLStack.pop();
+		LLStack.push(plus);
+		break;
+	}
+	case 35: // Opadd = minus
+	{
+		Syms minus = { "minus", true };
+
+		LLStack.pop();
+		LLStack.push(minus);
+		break;
+	}
+	case 36: // Opmul = aster
+	{
+		Syms aster = { "aster", true };
+
+		LLStack.pop();
+		LLStack.push(aster);
+		break;
+	}
+	case 37: // Opmul = slash
+	{
+		Syms slash = { "slash", true };
+
+		LLStack.pop();
+		LLStack.push(slash);
+		break;
+	}
+	case 38: // Opmul = caret
+	{
+		Syms caret = { "caret", true };
+
+		LLStack.pop();
+		LLStack.push(caret);
+		break;
+	}
+
+
 	}
 }
 
